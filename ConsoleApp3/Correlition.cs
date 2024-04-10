50eb5f5ca29769b4146acba08b7c35ad1d034701
Семён Власов <pkekepke@mail.ru>
2024-04-10 21:46:05 +0300

    init

---
+++ ConsoleApp3/Correlition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Correlition
+    {
+        public static double[] PearsonCorrelation(double[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int lenght = matrix.GetLength(1);
+            int m = matrix.GetLength(1);
+            double[,] correlations = new double[lenght, m];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    double sumX = 0, sumY = 0, sumXSq = 0, sumYSq = 0, sumXY = 0;
+                    for (int k = 0; k < n; k++)
+                    {
+                        double x = matrix[k, i];
+                        double y = matrix[k, j];
+                        sumX += x;
+                        sumY += y;
+                        sumXSq += Math.Pow(x, 2);
+                        sumYSq += Math.Pow(y, 2);
+                        sumXY += x * y;
+                    }
+                    double numerator = n * sumXY - sumX * sumY;
+                    double denominator = Math.Sqrt((n * sumXSq - Math.Pow(sumX, 2)) * (n * sumYSq - Math.Pow(sumY, 2)));
+                    correlations[i, j] = numerator / denominator;
+                }
+            }
+
+            double[] result = new double[m-1];
+            for (int i = 0; i < m-1; i++) {
+                result[i] = Math.Round(correlations[lenght-1, i], 4);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp3;
+using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+
+double[] matrix = {24,27, 26, 21  ,20, 31, 26, 22,20,18, 30, 29,24,26};
+
+double[] matrixy = {
+100,115,117,119,134, 94,105, 103,111,124, 122,109,110,86};
+
+double[] p = Fit.Polynomial(matrix, matrixy, 2);
+
+for (int i = 0; i < p.Length; i++) {
+
+    Console.WriteLine(p[i]);
+}
